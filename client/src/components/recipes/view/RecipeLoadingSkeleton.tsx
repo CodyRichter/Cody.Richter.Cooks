c385b4cf1c7cdc438930be14ce1c346f4d37f42a
Cody Richter <Cody@Richter.codes>
2025-10-13 18:52:51 -0700

    Update Recipe Loading Skeleton

--- client/src/components/recipes/view/RecipeLoadingSkeleton.tsx
+++ client/src/components/recipes/view/RecipeLoadingSkeleton.tsx
@@ -1,21 +1,48 @@
+import { Grid, Paper, Skeleton } from "@mantine/core";
+
 import React from "react";
-import { Skeleton } from "@mantine/core";
 
 export default function RecipeLoadingSkeleton() {
   return (
-    <>
-      <Skeleton height={10} mt={6} width="50%" radius="xl" />
-      <Skeleton height={10} mt={6} width="50%" radius="xl" />
-      <Skeleton height={10} mt={4} width="50%" radius="xl" />
+    <Grid>
+      <Paper
+        shadow="lg"
+        p="lg"
+        radius="md"
+        withBorder
+        mb="md"
+        style={{ borderLeft: "6px solid #e2a478", minHeight: "400px" }}
+        w="100%"
+      >
+        <Skeleton height={10} mt={6} width="50%" radius="xl" />
+        <Skeleton height={10} mt={6} width="50%" radius="xl" />
+        <Skeleton height={10} mt={4} width="50%" radius="xl" />
 
-      <Skeleton height={6} mt={20} width="90%" radius="xl" />
-      <Skeleton height={6} mt={10} width="90%" radius="xl" />
-      <Skeleton height={6} mt={10} width="90%" radius="xl" />
-      <Skeleton height={6} mt={10} width="90%" radius="xl" />
-      <Skeleton height={6} mt={10} width="90%" radius="xl" />
-      <Skeleton height={6} mt={10} width="90%" radius="xl" />
-      <Skeleton height={6} mt={10} width="90%" radius="xl" />
-      <Skeleton height={6} mt={10} width="55%" radius="xl" />
-    </>
+        <Skeleton height={6} mt={20} width="80%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+
+        <Skeleton height={6} mt={40} width="80%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+
+        <Skeleton height={6} mt={40} width="80%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+
+        <Skeleton height={6} mt={40} width="80%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+
+        <Skeleton height={6} mt={40} width="80%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+        <Skeleton height={6} mt={10} width="70%" radius="xl" />
+      </Paper>
+    </Grid>
   );
 }
