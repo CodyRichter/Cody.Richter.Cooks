9384513736dd486a7fa1074853ee4b2ebacb82ab
Cody Richter <Cody@Richter.codes>
2026-01-31 00:05:19 -0800

    Complete cutover: Remove legacy client and server directories

--- client/src/styles/theme.ts
+++
@@ -1,9 +0,0 @@
-import { MantineThemeOverride } from "@mantine/core";
-
-export default function getCodyRichterCooksTheme(
-  fontFamily: string
-): MantineThemeOverride {
-  return {
-    fontFamily: fontFamily,
-  };
-}
